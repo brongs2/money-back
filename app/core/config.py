143b81766e272a32963fb686868d3a1c15dfc41d
brongs2 <brongs2@kaist.ac.kr>
2025-08-18 10:57:58 +0900

    first

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DB_URL: str = "sqlite:///./dev.db"  # 나중에 Postgres로 바꾸기 쉬움
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
